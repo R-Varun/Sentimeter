0f5f6b768030ff5542cde0172c8a5b388aa875fb
xuranchen <xuran.chen@gmail.com>
2018-02-07 12:02:03 -0500

    update to py3

--- python/main.py
+++ python/main.py
@@ -17,7 +17,7 @@ if begin is -1 and end is -1:
     begin = 0
     end = len(input)
 elif begin < 0 or end > len(input):
-    print "invalid granularity"
+    print("invalid granularity")
     quit()
 
 for sentence in input[begin: end]:
