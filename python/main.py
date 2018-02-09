3813d62553242a0a41a9fd5b4a1404058e978ee9
xuranchen <xuran.chen@gmail.com>
2018-02-09 11:31:27 -0500

    stride implementation

--- python/main.py
+++ python/main.py
@@ -9,9 +9,9 @@ input = data[0]
 granularity = data[2]
 begin = int(granularity[0])
 end = int(granularity[1])
+stride = input[3]
 
 
-topicList = {}
 
 if begin is -1 and end is -1:
     begin = 0
@@ -20,19 +20,44 @@ elif begin < 0 or end > len(input):
     print("invalid granularity")
     quit()
 
-for sentence in input[begin: end]:
-    if "utterance" not in sentence:
-        continue
-    taggedSentences = contextsummary.posTag(sentence["utterance"])
-    topic = contextsummary.sentenctExtract(taggedSentences)
-    for top in topic:
-        if top in topicList:
-            topicList[top] = topicList[top] + 1
-        else:
-            topicList[top] = 1
 
-#print topicList
+contextList = []
+topicList = {}
+
+if stride is None:
+    for sentence in input[begin: end]:
+        if "utterance" not in sentence:
+            continue
+        taggedSentences = contextsummary.posTag(sentence["utterance"])
+        topic = contextsummary.sentenctExtract(taggedSentences)
+        for top in topic:
+            if top in topicList:
+                topicList[top] = topicList[top] + 1
+            else:
+                topicList[top] = 1
+else:
+    stride = int(stride)
+    counter = 0
+
+    for sentence in input[begin: end]:
+        if "utterance" not in sentence:
+            continue
+        taggedSentences = contextsummary.posTag(sentence["utterance"])
+        topic = contextsummary.sentenctExtract(taggedSentences)
+        for top in topic:
+            if top in topicList:
+                topicList[top] = topicList[top] + 1
+            else:
+                topicList[top] = 1
+        counter += 1
+        if counter >= stride:
+            counter = 0
+            sortedTopics = sorted(topicList, key = lambda x : -1 * topicList[x])
+            contextList.append(sortedTopics)
+            topicList = {}
 sortedTopics = sorted(topicList, key = lambda x : -1 * topicList[x])
+contextList.append(sortedTopics)
+
 """print(sortedTopics[:5])
 for x in sortedTopics[:5]:
     print(topicList[x])"""
